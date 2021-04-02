375aa86eda2e9723261c1a1b19d0d236025b950c
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2021-04-01 18:09:03 -0700

    test

--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -1,6 +1,18 @@
 const async = require("async");
 const Weapon = require("../models/weapon");
 
+// List all the weapons
 exports.weapon_list = function (req, res, next) {
-  async.parallel({});
+  async.parallel({
+    weapons: function (callback) {
+      Weapon.find({})
+        .sort([["name", "ascending"]])
+        .populate("categories")
+        .exec(callback);
+    },
+    // Successful, so render
+    res.render("weapon_list", {
+      title: "Weapon List",
+    })
+  });
 };
