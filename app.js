d0bfcb37fc28c5352222c7ce9e6069f2e46e89cd
SultanBadri <badris846@gmail.com>
2021-09-05 14:13:01 -0700

    test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/favicon.ico', express.static('./images/favicon.ico'));
+app.use("/favicon.ico", express.static("./images/favicon.ico"));
 
 app.use("/", indexRouter);
 app.use("/inventory", inventoryRouter);
